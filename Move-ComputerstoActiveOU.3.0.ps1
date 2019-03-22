02a171ff1fc8cf8b4a77afe0231b06ff41a5ce95
Justin Cooper <jcooper@chicousd.org>
2019-03-22 14:03:34 -0700

    modified for Jenkins

--- Move-ComputerstoActiveOU.3.0.ps1
+++ Move-ComputerstoActiveOU.3.0.ps1
@@ -11,45 +11,24 @@ param (
  [switch]$WhatIf
 )
 
-Write-Verbose "Running $($MyInvocation.MyCommand.Name)"
+# AD Domain Controller Session
+$adCmdLets = 'Get-ADcomputer','Move-ADObject'
+$adSession = New-PSSession -ComputerName $DomainController -Credential $ADCredential
+Import-PSSession -Session $adSession -Module ActiveDirectory -CommandName $adCmdLets -AllowClobber
 
-$SiteOULookupTable = Import-CSV ".\SiteOU-Lookuptable.1.0.csv"-Delim "|"
-$SourceOU = "CN=Computers,DC=CHICO,DC=USD"
-$TargetPath = "OU=KACE IMAGING,OU=STUDENTS,OU=DESKTOPS,OU=COMPUTERS,OU=Domain_Root,DC=chico,DC=usd"
-$Common = "\\mirage\Scripts\CommonFunctions"
+$sourceOU = "CN=Computers,DC=CHICO,DC=USD"
+$targetOU = "OU=KACE IMAGING,OU=STUDENTS,OU=DESKTOPS,OU=COMPUTERS,OU=Domain_Root,DC=chico,DC=usd"
 
-. "$Common\Check-ADModule.3.0.ps1"
-Check-ADModule $PDC = (Get-ADDomain).InfraStructureMaster
-$ComputerObjects = Get-ADcomputer -Filter * -SearchBase $SourceOU
+$endTime = "11:30pm"
+if (!$WhatIf) { "Running until $endTime" }
+do {
+ $computerObjs = Get-ADcomputer -Filter * -SearchBase $sourceOU
+ foreach ($obj in $computerObjs){
+  # Move Object to TargetPath
+  $obj | Move-ADObject -TargetPath $targetOU -Whatif:$WhatIf
+ }
+} until ( $WhatIf -or ( (Get-Date) -ge (Get-Date $endTime) ) )
 
-foreach ($Object in $ComputerObjects)
-{
-	$ObjectPrefix = $Object.Name.substring(0,3)
-	$TargetSite = $SiteOULookupTable | where { $_.SiteNameMask -eq "$ObjectPrefix" }
-	# Add SiteName to Description Attribute
-	if ($TargetSite)
-	{
-		Write-Verbose $TargetSite.SiteName
-		try
-		{
-			$Object | Set-AdObject -Desc $TargetSite.SiteName -Whatif:$WhatIf
-		}
-		catch
-		{
-			Write-Error -Exception "Unable to set attribute(s) for $($Object.Name)"
-		}
-	}
-	# Move Object to TargetPath
-	try 
-	{
-		$Object | Move-ADObject -TargetPath $TargetPath -Whatif:$WhatIf
-	}
-	catch
-	{
-		Write-Error -Exception "Unable to move $($Object.Name) to $TargetPath"
-	}
-	if ($Slow) { Write-Verbose "Pausing"; read-host }
-}
-
-
+Write-Verbose "Tearing down sessions"
+Get-PSSession | Remove-PSSession
  
